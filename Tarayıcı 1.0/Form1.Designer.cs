677f870b9b853e4d1593e7be90aa8b4c5c4a9e48
Phoique <enes-gta45@hotmail.com>
2017-01-17 23:57:54 +0300

    Version 1.0

---
+++ Tarayıcı 1.0/Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace Tarayıcı_1._0
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.bunifuImageButton1 = new ns1.BunifuImageButton();
+            this.bunifuImageButton2 = new ns1.BunifuImageButton();
+            this.bunifuImageButton3 = new ns1.BunifuImageButton();
+            this.bunifuImageButton4 = new ns1.BunifuImageButton();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.ara_btn = new ns1.BunifuFlatButton();
+            this.ara = new ns1.BunifuTextbox();
+            ((System.ComponentModel.ISupportInitialize)(this.bunifuImageButton1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bunifuImageButton2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bunifuImageButton3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bunifuImageButton4)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.webBrowser1.Location = new System.Drawing.Point(1, 33);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(1087, 448);
+            this.webBrowser1.TabIndex = 0;
+            this.webBrowser1.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser1_DocumentCompleted);
+            // 
+            // bunifuImageButton1
+            // 
+            this.bunifuImageButton1.BackColor = System.Drawing.Color.Transparent;
+            this.bunifuImageButton1.Image = ((System.Drawing.Image)(resources.GetObject("bunifuImageButton1.Image")));
+            this.bunifuImageButton1.ImageActive = null;
+            this.bunifuImageButton1.Location = new System.Drawing.Point(0, 3);
+            this.bunifuImageButton1.Name = "bunifuImageButton1";
+            this.bunifuImageButton1.Size = new System.Drawing.Size(33, 30);
+            this.bunifuImageButton1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.bunifuImageButton1.TabIndex = 2;
+            this.bunifuImageButton1.TabStop = false;
+            this.bunifuImageButton1.Zoom = 10;
+            this.bunifuImageButton1.Click += new System.EventHandler(this.bunifuImageButton1_Click);
+            // 
+            // bunifuImageButton2
+            // 
+            this.bunifuImageButton2.BackColor = System.Drawing.Color.Transparent;
+            this.bunifuImageButton2.Image = ((System.Drawing.Image)(resources.GetObject("bunifuImageButton2.Image")));
+            this.bunifuImageButton2.ImageActive = null;
+            this.bunifuImageButton2.Location = new System.Drawing.Point(33, 3);
+            this.bunifuImageButton2.Name = "bunifuImageButton2";
+            this.bunifuImageButton2.Size = new System.Drawing.Size(33, 30);
+            this.bunifuImageButton2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.bunifuImageButton2.TabIndex = 3;
+            this.bunifuImageButton2.TabStop = false;
+            this.bunifuImageButton2.Zoom = 10;
+            this.bunifuImageButton2.Click += new System.EventHandler(this.bunifuImageButton2_Click);
+            // 
+            // bunifuImageButton3
+            // 
+            this.bunifuImageButton3.BackColor = System.Drawing.Color.Transparent;
+            this.bunifuImageButton3.Image = ((System.Drawing.Image)(resources.GetObject("bunifuImageButton3.Image")));
+            this.bunifuImageButton3.ImageActive = null;
+            this.bunifuImageButton3.Location = new System.Drawing.Point(66, 3);
+            this.bunifuImageButton3.Name = "bunifuImageButton3";
+            this.bunifuImageButton3.Size = new System.Drawing.Size(33, 30);
+            this.bunifuImageButton3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.bunifuImageButton3.TabIndex = 4;
+            this.bunifuImageButton3.TabStop = false;
+            this.bunifuImageButton3.Zoom = 10;
+            this.bunifuImageButton3.Click += new System.EventHandler(this.bunifuImageButton3_Click);
+            // 
+            // bunifuImageButton4
+            // 
+            this.bunifuImageButton4.BackColor = System.Drawing.Color.Transparent;
+            this.bunifuImageButton4.Image = ((System.Drawing.Image)(resources.GetObject("bunifuImageButton4.Image")));
+            this.bunifuImageButton4.ImageActive = null;
+            this.bunifuImageButton4.Location = new System.Drawing.Point(95, 3);
+            this.bunifuImageButton4.Name = "bunifuImageButton4";
+            this.bunifuImageButton4.Size = new System.Drawing.Size(33, 30);
+            this.bunifuImageButton4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.bunifuImageButton4.TabIndex = 5;
+            this.bunifuImageButton4.TabStop = false;
+            this.bunifuImageButton4.Zoom = 10;
+            this.bunifuImageButton4.Click += new System.EventHandler(this.bunifuImageButton4_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(242)))), ((int)(((byte)(242)))), ((int)(((byte)(242)))));
+            this.panel1.Controls.Add(this.ara_btn);
+            this.panel1.Controls.Add(this.ara);
+            this.panel1.Controls.Add(this.bunifuImageButton1);
+            this.panel1.Controls.Add(this.bunifuImageButton4);
+            this.panel1.Controls.Add(this.bunifuImageButton2);
+            this.panel1.Controls.Add(this.bunifuImageButton3);
+            this.panel1.Location = new System.Drawing.Point(1, 2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1087, 37);
+            this.panel1.TabIndex = 7;
+            // 
+            // ara_btn
+            // 
+            this.ara_btn.Activecolor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(139)))), ((int)(((byte)(87)))));
+            this.ara_btn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.ara_btn.BackColor = System.Drawing.Color.MediumSeaGreen;
+            this.ara_btn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ara_btn.BorderRadius = 0;
+            this.ara_btn.ButtonText = "Ara";
+            this.ara_btn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.ara_btn.DisabledColor = System.Drawing.Color.Gray;
+            this.ara_btn.Iconcolor = System.Drawing.Color.Transparent;
+            this.ara_btn.Iconimage = ((System.Drawing.Image)(resources.GetObject("ara_btn.Iconimage")));
+            this.ara_btn.Iconimage_right = null;
+            this.ara_btn.Iconimage_right_Selected = null;
+            this.ara_btn.Iconimage_Selected = null;
+            this.ara_btn.IconMarginLeft = 0;
+            this.ara_btn.IconMarginRight = 0;
+            this.ara_btn.IconRightVisible = true;
+            this.ara_btn.IconRightZoom = 0D;
+            this.ara_btn.IconVisible = true;
+            this.ara_btn.IconZoom = 70D;
+            this.ara_btn.IsTab = false;
+            this.ara_btn.Location = new System.Drawing.Point(1022, 3);
+            this.ara_btn.Name = "ara_btn";
+            this.ara_btn.Normalcolor = System.Drawing.Color.MediumSeaGreen;
+            this.ara_btn.OnHovercolor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(129)))), ((int)(((byte)(77)))));
+            this.ara_btn.OnHoverTextColor = System.Drawing.Color.White;
+            this.ara_btn.selected = false;
+            this.ara_btn.Size = new System.Drawing.Size(62, 32);
+            this.ara_btn.TabIndex = 8;
+            this.ara_btn.Text = "Ara";
+            this.ara_btn.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.ara_btn.Textcolor = System.Drawing.Color.White;
+            this.ara_btn.TextFont = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ara_btn.Click += new System.EventHandler(this.bunifuFlatButton1_Click);
+            // 
+            // ara
+            // 
+            this.ara.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ara.BackColor = System.Drawing.Color.White;
+            this.ara.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("ara.BackgroundImage")));
+            this.ara.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ara.ForeColor = System.Drawing.Color.Black;
+            this.ara.Icon = ((System.Drawing.Image)(resources.GetObject("ara.Icon")));
+            this.ara.Location = new System.Drawing.Point(134, 3);
+            this.ara.Name = "ara";
+            this.ara.Size = new System.Drawing.Size(882, 30);
+            this.ara.TabIndex = 9;
+            this.ara.text = "";
+            this.ara.OnTextChange += new System.EventHandler(this.ara_OnTextChange);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1089, 483);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.webBrowser1);
+            this.Name = "Form1";
+            this.Text = "Pho Tarayıcı 1.0";
+            ((System.ComponentModel.ISupportInitialize)(this.bunifuImageButton1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bunifuImageButton2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bunifuImageButton3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bunifuImageButton4)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private ns1.BunifuImageButton bunifuImageButton1;
+        private ns1.BunifuImageButton bunifuImageButton2;
+        private ns1.BunifuImageButton bunifuImageButton3;
+        private ns1.BunifuImageButton bunifuImageButton4;
+        private System.Windows.Forms.Panel panel1;
+        private ns1.BunifuTextbox ara;
+        private ns1.BunifuFlatButton ara_btn;
+    }
+}
+
---
+++ Tarayıcı 1.0/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tarayıcı_1._0
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void bunifuImageButton1_Click(object sender, EventArgs e)
+        {
+            webBrowser1.GoBack();
+        }
+
+        private void bunifuImageButton2_Click(object sender, EventArgs e)
+        {
+            webBrowser1.GoForward();
+        }
+
+        private void bunifuImageButton3_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Refresh();
+        }
+
+        private void bunifuFlatButton1_Click(object sender, EventArgs e)
+        {
+            if (ara.text == "")
+            {
+                MessageBox.Show("Lütfen URL kısmını boş bırakmayın.", "URL KISMI BOŞ");
+            }
+            else
+            {
+                string url= ara.text;
+                webBrowser1.Navigate(url);
+            }
+            //switch (ara.text)
+            //{
+            //    case "google":
+            //        {
+            //            ara.text = "www.google.com.tr";
+            //            webBrowser1.Navigate("www.google.com.tr");
+            //            break;
+            //        }
+            //    case "facebook":
+            //        {
+            //            webBrowser1.Navigate("www.facebook.com.tr");
+            //            break;
+            //        }
+            //    case "tht":
+            //        {
+            //            webBrowser1.Navigate("www.turkhackteam.org");
+            //            break;
+            //        }
+            //    default:
+            //        break;
+            //}
+        }
+
+        private void bunifuImageButton4_Click(object sender, EventArgs e)
+        {
+            string url = "www.google.com.tr";
+            webBrowser1.Navigate(url);
+        }
+
+        private void ara_OnTextChange(object sender, EventArgs e)
+        {
+            switch (ara.text)
+            {
+                case "google":
+                    {
+                        ara.text = "www.google.com.tr";
+                        webBrowser1.Navigate("www.google.com.tr");
+                        break;
+                    }
+                case "facebook":
+                    {
+                        ara.text = "www.facebook.com.tr";
+                        webBrowser1.Navigate("www.facebook.com.tr");
+                        break;
+                    }
+                case "yandex":
+                    {
+                        ara.text = "www.yandex.com.tr";
+                        webBrowser1.Navigate("www.yandex.com.tr");
+                        break;
+                    }
+                default:
+                    break;
+            }
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+
+        }
+    }
+}
